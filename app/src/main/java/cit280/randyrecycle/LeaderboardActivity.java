ed8752214015b89723d7c0156f4d41545140dc70
furlopunk <LYtuh4Sc2Rsm>
2016-10-11 12:56:43 -0400

    Created activities for options menu and leaderboard. I also linked the buttons to the activities.

---
+++ app/src/main/java/cit280/randyrecycle/LeaderboardActivity.java
@@ -0,0 +1,24 @@
+package cit280.randyrecycle;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LeaderboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Leaderboard");
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
